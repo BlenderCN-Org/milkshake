1dcf1f27a28f4ffabd90d7454620737fa09837f2
Sam Van Hulle <hello@samvanhulle.be>
2018-11-07 23:25:33 +0100

    added render settings, more layer setup for 2.8

--- lighting_functions.py
+++ lighting_functions.py
@@ -22,64 +22,95 @@ def read_materials(self, context:bpy.types.Context):
 
 
 def render_setup(context:bpy.types.Context):
-    """Sets up the view layers."""
+    """Sets up the view layers and render settings."""
+
+    context.scene.render.engine = "CYCLES"
+    context.scene.display_settings.display_device = "sRGB"
+    context.scene.view_settings.view_transform = "Filmic"
+    context.scene.view_settings.look = "Filmic - Base Contrast"
+    context.scene.sequencer_colorspace_settings.name = "Filmic Log"
+    context.scene.cycles.samples = 250
+    context.scene.cycles.preview_samples = 0
+    context.scene.cycles.sample_clamp_indirect = 10
+    context.scene.cycles.max_bounces = 12
+    context.scene.cycles.diffuse_bounces = 3
+    context.scene.cycles.glossy_bounces = 3
+    context.scene.cycles.transparent_max_bounces = 12
+    context.scene.cycles.transmission_bounces = 3
+    context.scene.cycles.volume_bounces = 0
+    context.scene.cycles.blur_glossy = 2
+    context.scene.cycles.caustics_reflective = False
+    context.scene.cycles.caustics_refractive = False
+    context.scene.cycles.use_motion_blur = False
+    context.scene.cycles.film_transparent = True
 
     collections = bpy.data.collections
     layers = context.scene.view_layers
 
     # Set up collections
-    animprops               = collections.new("Animprops")
-    bg01_sets               = collections.new("BG01 Sets")
-    bg02_sets               = collections.new("BG02 Sets")
-    characters              = collections.new("Characters")
-    dust                    = collections.new("DUST")
-    lights                  = collections.new("BG + FG Lights")
-    bg_lights               = collections.new("BG Lights")
-    fg_lights               = collections.new("FG Lights")
-    vol_lights              = collections.new("VOL Lights")
-    hidden                  = collections.new("Hidden")
+    for c in collections:
+        collections.remove(c)
+    new_collections = {
+        "animprops"               : collections.new("Animprops"),
+        "bg01_sets"               : collections.new("BG01 Sets"),
+        "bg02_sets"               : collections.new("BG02 Sets"),
+        "characters"              : collections.new("Characters"),
+        "dust"                    : collections.new("DUST"),
+        "lights"                  : collections.new("BG + FG Lights"),
+        "bg_lights"               : collections.new("BG Lights"),
+        "fg_lights"               : collections.new("FG Lights"),
+        "vol_lights"              : collections.new("VOL Lights"),
+        "hidden"                  : collections.new("Hidden")
+    }
+    for c in new_collections:
+        context.scene.link(c)
 
     # Set up view layers
     layer_templates = [
         {
             "name": "VOL",
-            "exclude": [dust, lights, bg_lights, fg_lights, hidden],
-            "filter": ["ao", "sky"],
+            "exclude": ["dust", "lights", "bg_lights", "fg_lights", "hidden"],
+            "filter": ["solid", "strand"],
             "passes": ["combined"],
             "default": True
         },
         {
             "name": "DUST",
-            "indirect_only": [animprops, bg01_sets, bg02_sets, characters],
-            "exclude": [hidden, vol_lights],
-            "holdout": [animprops, bg01_sets, bg02_sets, characters],
-            "filter": ["ao"],
+            "indirect_only": ["animprops", "bg01_sets", "bg02_sets", "characters"],
+            "exclude": ["hidden", "vol_lights"],
+            "holdout": ["animprops", "bg01_sets", "bg02_sets", "characters"],
+            "filter": ["sky", "solid", "strand"],
             "passes": ["combined", "z", "vector"],
             "default": False
         },
         {
             "name": "FG",
-            "indirect_only": [bg01_sets, bg02_sets],
-            "exclude": [dust, bg_lights, vol_lights, hidden],
-            "holdout": [bg01_sets, bg02_sets],
-            "filter": [],
-            "passes": ["combined", "z", "vector", "subsurface_direct", "mist", "ambient_occlusion", "crypto_material"],
+            "indirect_only": ["bg01_sets", "bg02_sets"],
+            "exclude": ["dust", "bg_lights", "vol_lights", "hidden"],
+            "holdout": ["bg01_sets", "bg02_sets"],
+            "filter": ["sky", "ao", "solid", "strand"],
+            "passes": ["combined", "z", "vector", "subsurface_direct", "mist", "ambient_occlusion"],
+            "crypto": ["object", "material"],
             "default": True
         },
         {
             "name": "BG01",
-            "indirect_only": [animprops, characters],
-            "exclude": [dust, fg_lights, hidden, vol_lights],
-            "holdout": [bg02_sets],
-            "passes": ["combined", "z", "vector", "mist", "ambient_occlusion", "crypto_material"],
+            "indirect_only": ["animprops", "characters"],
+            "exclude": ["dust", "fg_lights", "hidden", "vol_lights"],
+            "holdout": ["bg02_sets"],
+            "filter": ["sky", "ao", "solid", "strand"],
+            "passes": ["combined", "z", "vector", "mist", "ambient_occlusion"],
+            "crypto": ["object", "material"],
             "default": True
         },
         {
             "name": "BG02",
-            "indirect_only": [animprops, bg01_sets, characters],
-            "exclude": [dust, fg_lights, hidden, vol_lights],
+            "indirect_only": ["animprops", "bg01_sets", "characters"],
+            "exclude": ["dust", "fg_lights", "hidden", "vol_lights"],
             "holdout": [],
-            "passes": ["combined", "z", "vector", "mist", "ambient_occlusion", "crypto_material"],
+            "filter": ["sky", "ao", "solid", "strand"],
+            "passes": ["combined", "z", "vector", "mist", "ambient_occlusion"],
+            "crypto": ["object", "material"],
             "default": True
         }
     ]
@@ -100,18 +131,24 @@ def render_setup(context:bpy.types.Context):
         layer.name = template["name"]
         if "indirect_only" in template:
             # Adapt to 2.80
+            pass
         if "exclude" in template:
             # Adapt to 2.80
+            pass
         if "holdout" in template:
             # Adapt to 2.80
+            pass
+        if "filter" in template:
+            filters = ["sky", "ao", "solid", "strand", "freestyle"]
+            for d in filters:
+                setattr(layer, "use_{}".format(d), False)
+            for d in template["filter"]:
+                setattr(layer, "use_{}".format(d), True)
         if "passes" in template:
             layer.use_pass_combined = False
             layer.use_pass_z = False
             for p in template["passes"]:
                 setattr(layer, "use_pass_{}".format(p), True)
-        if "filter" in template:
-            for d in template["filter"]:
-                setattr(layer, "use_{}".format(d), False)
         if "default" in template:
             layer.use = template["default"]
     layers.remove(old)
