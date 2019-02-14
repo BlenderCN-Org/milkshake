d46b711f9315d44d5ccdd55c902503e2349c1e6c
Sam Van Hulle <hello@samvanhulle.be>
2019-02-14 12:01:04 +0100

    finished Transfer Transforms; did some cleanup

--- modeling.py
+++ modeling.py
@@ -29,12 +29,11 @@ class MODELING_UL_transfer_transforms(bpy.types.UIList):
     bl_idname = "milkshake.modeling_ul_transfer_transforms"
 
     def draw_item(context, layout, data, item, icon, active_data, active_property):
-        print(item)
-        if layout.type in {"DEFAULT", "COMPACT"}:
+        if layout.type in {'DEFAULT', 'COMPACT'}:
             # We use icon_value of label, as our given icon is an integer value, not an enum ID.
             layout.label(text = item.obj_name, icon_value = icon)
-        elif layout.type in {"GRID"}:
-            layout.alignment = "CENTER"
+        elif layout.type in {'GRID'}:
+            layout.alignment = 'CENTER'
             layout.label(text = "", icon_value = icon)
 
 
@@ -48,13 +47,14 @@ class MODELING_OT_add_selection_to_tt_set(bpy.types.Operator):
 
     bl_idname = "milkshake.modeling_ot_add_selection_to_tt_set"
     bl_label = "Add Selection"
-    bl_options = {"REGISTER", "UNDO"}
+    bl_options = {'REGISTER'}
 
-    tt_set : bpy.props.EnumProperty(items = [("a", "A", "List A"), ("b", "B", "List B")])
+    tt_set: bpy.props.EnumProperty(items = [('a', 'A', 'List A'), ('b', 'B', 'List B')])
 
     def execute(self, context):
-        func.add_selection_to_tt_set(context, tt_set = self.tt_set)
-        return {"FINISHED"}
+        func.clear_tt_set(context = context, tt_set = self.tt_set)
+        func.add_selection_to_tt_set(context = context, tt_set = self.tt_set)
+        return {'FINISHED'}
 
 
 class MODELING_OT_clear_sharp(bpy.types.Operator):
@@ -62,13 +62,25 @@ class MODELING_OT_clear_sharp(bpy.types.Operator):
 
     bl_idname = "milkshake.modeling_ot_clear_sharp"
     bl_label = "Remove Sharp Edges"
-    bl_options = {"REGISTER", "UNDO"}
-
-    selection_only : bpy.props.BoolProperty(name = "Selection Only", default = False)
+    bl_options = {'REGISTER', 'UNDO'}
 
     def execute(self, context):
-        func.clear_sharp(context, selection_only = self.selection_only)
-        return {"FINISHED"}
+        func.clear_sharp(context = context)
+        return {'FINISHED'}
+
+
+class MODELING_OT_clear_tt_set(bpy.types.Operator):
+    """Clear all sharp edges in meshes.\nOn selection or everything"""
+
+    bl_idname = "milkshake.modeling_ot_clear_tt_set"
+    bl_label = "Clear List"
+    bl_options = {'REGISTER'}
+
+    tt_set: bpy.props.EnumProperty(items = [('a', 'A', 'List A'), ('b', 'B', 'List B')])
+
+    def execute(self, context):
+        func.clear_tt_set(context = context, tt_set = self.tt_set)
+        return {'FINISHED'}
 
 
 class MODELING_OT_generate_placeholders(bpy.types.Operator):
@@ -76,25 +88,11 @@ class MODELING_OT_generate_placeholders(bpy.types.Operator):
 
     bl_idname = "milkshake.modeling_ot_generate_placeholders"
     bl_label = "Generate Placeholders"
-    bl_options = {"REGISTER", "UNDO"}
+    bl_options = {'REGISTER'}
 
     def execute(self, context):
-        func.generate_placeholders(context)
-        return {"FINISHED"}
-
-
-class MODELING_OT_reset_viewport_display(bpy.types.Operator):
-    """Reset viewport display properties on objects.\nOn selection or everything"""
-
-    bl_idname = "milkshake.modeling_ot_reset_viewport_display"
-    bl_label = "Remove Drawing Overrides"
-    bl_options = {"REGISTER", "UNDO"}
-
-    selection_only : bpy.props.BoolProperty(name = "Selection Only", default = False)
-
-    def execute(self, context):
-        func.reset_viewport_display(context, selection_only = self.selection_only)
-        return {"FINISHED"}
+        func.generate_placeholders(context = context)
+        return {'FINISHED'}
 
 
 class MODELING_OT_select_unsubdivided(bpy.types.Operator):
@@ -102,11 +100,11 @@ class MODELING_OT_select_unsubdivided(bpy.types.Operator):
 
     bl_idname = "milkshake.modeling_ot_select_unsubdivided"
     bl_label = "Select Unsubdivided"
-    bl_options = {"REGISTER", "UNDO"}
+    bl_options = {'REGISTER'}
 
     def execute(self, context):
-        func.select_unsubdivided(context, selection_only = self.selection_only)
-        return {"FINISHED"}
+        func.select_unsubdivided(context = context)
+        return {'FINISHED'}
 
 
 class MODELING_OT_set_subdivision(bpy.types.Operator):
@@ -114,17 +112,16 @@ class MODELING_OT_set_subdivision(bpy.types.Operator):
 
     bl_idname = "milkshake.modeling_ot_set_subdivision"
     bl_label = "Set Subdivisions"
-    bl_options = {"REGISTER","UNDO"}
+    bl_options = {'REGISTER', 'UNDO'}
 
-    selection_only : bpy.props.BoolProperty(name = "Selection Only", default = False)
-    iterations : bpy.props.IntProperty(name = "Iterations", default = 1, min = 0, max = 11)
+    iterations: bpy.props.IntProperty(name = "Iterations", default = 1, min = 0, max = 11)
 
     def invoke(self, context, event):
         return context.window_manager.invoke_props_dialog(self)
 
     def execute(self, context):
-        func.set_subdivision(context, iterations = self.iterations, selection_only = self.selection_only)
-        return {"FINISHED"}
+        func.set_subdivision(context = context, iterations = self.iterations)
+        return {'FINISHED'}
 
 
 class MODELING_OT_set_subdivision_to_adaptive(bpy.types.Operator):
@@ -132,13 +129,15 @@ class MODELING_OT_set_subdivision_to_adaptive(bpy.types.Operator):
 
     bl_idname = "milkshake.modeling_ot_set_subdivision_to_adaptive"
     bl_label = "To Adaptive"
-    bl_options = {"REGISTER", "UNDO"}
-
-    selection_only : bpy.props.BoolProperty(name = "Selection Only", default = False)
+    bl_options = {'REGISTER', 'UNDO'}
 
     def execute(self, context):
-        func.set_subdivision_to_adaptive(context, selection_only = self.selection_only)
-        return {"FINISHED"}
+        try:
+            func.set_subdivision_to_adaptive(context = context)
+            return {'FINISHED'}
+        except Exception as e:
+            self.report(type = {'ERROR'}, message = str(e))
+            return {'CANCELLED'}
 
 
 class MODELING_OT_transfer_transforms(bpy.types.Operator):
@@ -146,13 +145,15 @@ class MODELING_OT_transfer_transforms(bpy.types.Operator):
 
     bl_idname = "milkshake.modeling_ot_transfer_transforms"
     bl_label = "Transfer Transforms"
-    bl_options = {"REGISTER", "UNDO"}
+    bl_options = {'REGISTER', 'UNDO'}
 
     def execute(self, context):
-        if not func.transfer_transforms(context):
-            self.report(type = {"ERROR"}, message = "Both sets of objects must have equal length.")
-            return {"CANCELLED"}
-        return {"FINISHED"}
+        try:
+            func.transfer_transforms(context = context)
+            return {'FINISHED'}
+        except Exception as e:
+            self.report(type = {'ERROR'}, message = str(e))
+            return {'CANCELLED'}
 
 
 ##############################################################################
@@ -181,17 +182,26 @@ class PROPERTIES_PT_modeling(bpy.types.Panel):
         sub.operator("milkshake.modeling_ot_set_subdivision", icon = "MOD_SUBSURF")
         sub.operator("milkshake.modeling_ot_set_subdivision_to_adaptive")
 
-        col = lay.column(align = True)
-        sub = col.row(align = True)
-        col_a = sub.column(align = True)
-        col_a.operator("milkshake.modeling_ot_add_selection_to_tt_set").tt_set = "a"
-        col_a.template_list("UI_UL_list", "milkshake_tt_set_a", context.scene, "milkshake_tt_set_a", context.scene, "milkshake_tt_set_a_index")
-        col_b = sub.column(align = True)
-        col_b.operator("milkshake.modeling_ot_add_selection_to_tt_set").tt_set = "b"
-        col_b.template_list("UI_UL_list", "milkshake_tt_set_b", context.scene, "milkshake_tt_set_b", context.scene, "milkshake_tt_set_b_index")
-        row_button = col.row(align = True)
-        row_button.scale_y = 1.5
-        row_button.operator("milkshake.modeling_ot_transfer_transforms", icon = "TRIA_RIGHT")
+        # Transfer Transforms
+        lay.label(text = "Transfer Transforms:")
+        row = lay.row()
+
+        box_a = row.box()
+        sub = box_a.row(align = True)
+        sub.label(text = f"From: ({len(context.scene.milkshake_tt_set_a)})")
+        sub.operator("milkshake.modeling_ot_clear_tt_set", icon = "X", text = "").tt_set = 'a'
+        box_a.operator("milkshake.modeling_ot_add_selection_to_tt_set", text = f"Selection", icon = "SORT_DESC").tt_set = 'a'
+
+        column_button = row.column()
+        column_button.scale_x = 1.25
+        column_button.scale_y = 2.85
+        column_button.operator("milkshake.modeling_ot_transfer_transforms", icon = "TRIA_RIGHT", text = "")
+
+        box_b = row.box()
+        sub = box_b.row(align = True)
+        sub.label(text = f"To: ({len(context.scene.milkshake_tt_set_b)})")
+        sub.operator("milkshake.modeling_ot_clear_tt_set", icon = "X", text = "").tt_set = 'b'
+        box_b.operator("milkshake.modeling_ot_add_selection_to_tt_set", text = f"Selection", icon = "SORT_DESC").tt_set = 'b'
 
 
 ##############################################################################
@@ -203,8 +213,8 @@ classes = [
     MilkshakeSceneObject,
     MODELING_OT_add_selection_to_tt_set,
     MODELING_OT_clear_sharp,
+    MODELING_OT_clear_tt_set,
     MODELING_OT_generate_placeholders,
-    MODELING_OT_reset_viewport_display,
     MODELING_OT_select_unsubdivided,
     MODELING_OT_set_subdivision,
     MODELING_OT_set_subdivision_to_adaptive,
--- modeling_functions.py
+++ modeling_functions.py
@@ -12,22 +12,26 @@ from . import core_functions as core
 ##############################################################################
 
 
-def add_selection_to_tt_set(context, tt_set):
+def add_selection_to_tt_set(context = None, tt_set = None):
     """Add selection of objects to the transfer transforms lists"""
 
-    for obj in context.selected_objects:
-        if tt_set == "a":
+    if tt_set == 'a':
+        for obj in context.selected_objects:
             item = context.scene.milkshake_tt_set_a.add()
             item.obj_name = obj.name
-        elif tt_set == "b":
+    elif tt_set == 'b':
+        for obj in context.selected_objects:
             item = context.scene.milkshake_tt_set_b.add()
             item.obj_name = obj.name
+    else:
+        return False
+    return True
 
 
-def clear_sharp(context, selection_only):
+def clear_sharp(context = None):
     """Clear all sharp edges in meshes.\nOn selection or everything"""
 
-    if selection_only:
+    if len(context.selected_objects) > 0:
         objects = context.selected_objects
     else:
         objects = bpy.data.objects
@@ -36,10 +40,18 @@ def clear_sharp(context, selection_only):
     for mesh in meshes:
         for edge in mesh.data.edges:
             edge.use_edge_sharp = False
-        core.log(f"Cleared sharp edges for {mesh.data.name}")
 
 
-def generate_placeholders(context):
+def clear_tt_set(context = None, tt_set = None):
+    if tt_set == 'a':
+        context.scene.milkshake_tt_set_a.clear()
+    elif tt_set == 'b':
+        context.scene.milkshake_tt_set_b.clear()
+    else:
+        return False
+
+
+def generate_placeholders(context = None):
     """Generate empty placeholders for the selected objects"""
 
     if "Placeholders" in context.scene.collection.children.keys():
@@ -50,8 +62,8 @@ def generate_placeholders(context):
 
     for original in context.selected_objects:
         keyword = re.sub(r"\.[\d]+$", "", original.name)
-        if f"{keyword}.placeholders" in main_collection.children:
-            group = main_collection.children[f"{keyword}.placeholders"]
+        if f'{keyword}.placeholders' in main_collection.children:
+            group = main_collection.children[f'{keyword}.placeholders']
         else:
             group = bpy.data.collections.new(f"{keyword}.placeholders")
             main_collection.children.link(group)
@@ -60,60 +72,50 @@ def generate_placeholders(context):
         placeholder.rotation_euler = original.rotation_euler
         placeholder.scale = original.scale
         group.objects.link(placeholder)
-        core.log(f"Created placeholder {placeholder.name}")
 
 
-def reset_viewport_display(context, selection_only):
-    """Reset viewport display properties on objects.\nOn selection or everything"""
-
-    if selection_only:
-        objects = context.selected_objects
-    else:
-        objects = bpy.data.objects
-
-
-def select_unsubdivided(context):
+def select_unsubdivided(context = None):
     """Select all objects with a mesh data block and no subdivisions"""
 
-    bpy.ops.object.select_all(action = "DESELECT")
-    meshes = [obj for obj in bpy.data.objects if obj.type == "MESH"]
+    bpy.ops.object.select_all(action = 'DESELECT')
+    meshes = [obj for obj in bpy.data.objects if obj.type == 'MESH']
     for mesh in meshes:
         has_enabled_subsurf_modifiers = False
         for mod in mesh.modifiers:
-            if mod.type == "SUBSURF":
+            if mod.type == 'SUBSURF':
                 has_enabled_subsurf_modifiers = mod.show_render and (mod.render_levels > 0 or mesh.cycles.use_adaptive_subdivision)
                 break
         mesh.select = not has_enabled_subsurf_modifiers
 
 
-def set_subdivision(context, iterations, selection_only):
+def set_subdivision(context = None, iterations = None):
     """Add or set subdivision iterations for meshes.\nOn selection or everything"""
 
-    if selection_only:
+    if len(context.selected_objects) > 0:
         objects = context.selected_objects
     else:
         objects = bpy.data.objects
 
-    meshes = [obj for obj in objects if obj.type == "MESH"]
+    meshes = [obj for obj in objects if obj.type == 'MESH']
     for mesh in meshes:
         for mod in mesh.modifiers:
-            if mod.type == "SUBSURF":
+            if mod.type == 'SUBSURF':
                 mesh.modifiers.remove(mod)
         bpy.ops.object.shade_flat()
         if iterations > 0:
-            subsurf = mesh.modifiers.new(name = "Smooth Mesh", type = "SUBSURF")
+            subsurf = mesh.modifiers.new(name = "Smooth Mesh", type = 'SUBSURF')
             subsurf.levels = 1
             subsurf.render_levels = iterations
-            subsurf.uv_smooth = "PRESERVE_CORNERS"
+            subsurf.uv_smooth = 'PRESERVE_CORNERS'
             # context.scene.objects.active = mesh <--- doesn't work in 2.8
             bpy.ops.object.shade_smooth()
         core.log(f"Set subdivision for {mesh.name} to {iterations}")
 
 
-def set_subdivision_to_adaptive(context, selection_only):
+def set_subdivision_to_adaptive(context = None):
     """Set subdivision to adaptive for meshes.\nOn selection or everything"""
 
-    if selection_only:
+    if len(context.selected_objects) > 0:
         objects = context.selected_objects
     else:
         objects = bpy.data.objects
@@ -123,16 +125,27 @@ def set_subdivision_to_adaptive(context, selection_only):
         core.log(f"Enabled adaptive subdivision for {obj.name}")
 
 
-def transfer_transforms(context):
+def transfer_transforms(context = None):
     """Copy transformation values from a set of objects to another"""
 
-    if len(context.scene.milkshake_tt_set_a) == len(context.scene.milkshake_tt_set_b):
-        for index, obj in enumerate(context.scene.milkshake_tt_set_b):
-            context.scene.objects[obj.obj_name].location = context.scene.objects[context.scene.milkshake_tt_set_a[index].obj_name].location
-            context.scene.objects[obj.obj_name].rotation_euler = context.scene.objects[context.scene.milkshake_tt_set_a[index].obj_name].rotation_euler
-            context.scene.objects[obj.obj_name].scale = context.scene.objects[context.scene.milkshake_tt_set_a[index].obj_name].scale
-        context.scene.milkshake_tt_set_a.clear()
-        context.scene.milkshake_tt_set_b.clear()
-        return True
+    set_a = context.scene.milkshake_tt_set_a
+    set_b = context.scene.milkshake_tt_set_b
+
+    if len(set_a) != len(set_b):
+        raise ValueError("Both sets of objects must have equal length.")
+
     else:
-        return False
+
+        for ob in set_a:
+            if ob.obj_name not in context.scene.objects.keys():
+                raise ReferenceError(f"Object '{ob.obj_name}' doesn't exist anymore.")
+        for ob in set_b:
+            if ob.obj_name not in context.scene.objects.keys():
+                raise ReferenceError(f"Object '{ob.obj_name}' doesn't exist anymore.")
+
+        for index, ob in enumerate(set_b):
+            object_a = context.scene.objects[set_a[index].obj_name]
+            object_b = context.scene.objects[ob.obj_name]
+            object_b.location = object_a.location
+            object_b.rotation_euler = object_a.rotation_euler
+            object_b.scale = object_a.scale
